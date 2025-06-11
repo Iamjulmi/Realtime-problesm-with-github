1a5ee4b22f3aa242dbdea93d20f7ce1df3db6ba5
Tarun Julmi <julmetech@gmail.com>
2025-06-11 10:56:36 +0000

    added two files at a time from src folder

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to Learn Git with Tarun</h1>
+        <p>Master Git workflows with hands-on projects.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
+
